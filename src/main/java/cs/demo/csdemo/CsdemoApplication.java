445663114946dddee248beb67feb3f129a0a7fec
Sunil Bidgar <s.bidgar@gmail.com>
2022-07-30 18:51:24 +0530

    Added spring boot project

---
+++ src/main/java/cs/demo/csdemo/CsdemoApplication.java
@@ -0,0 +1,13 @@
+package cs.demo.csdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cs/demo/csdemo/CsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.demo.csdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
